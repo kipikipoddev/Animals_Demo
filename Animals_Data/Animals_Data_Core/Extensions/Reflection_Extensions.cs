2202fbdba4f4d97f9b7001c25e5f62392b0d1ac1
kipikipoddev <kipi.kipod.dev@gmail.com>
2026-05-22 21:12:29 +0300

    Revert "."
    This reverts commit 0239e689f0ca6a3ee96c890d998363ab7a42e10b.

--- Animals_Data/Animals_Data_Core/Extensions/Reflection_Extensions.cs
+++ Animals_Data/Animals_Data_Core/Extensions/Reflection_Extensions.cs
@@ -6,29 +6,29 @@ public static class Reflection_Extensions
 {
     public static IEnumerable<object> Get_Assembly_Objects(
         this Assembly assembly,
-        Type interface_type
-    ) => assembly.Get_Interface_Types(interface_type).Select(Activator.CreateInstance)!;
+        params Type[] interface_types
+    ) => assembly.Get_Interface_Types(interface_types).Select(Activator.CreateInstance)!;
 
-    public static bool Is_Interface<T>(this object obj, Type interface_type)
+    public static bool Is_Interface<T>(this object obj, params Type[] interface_types)
         where T : Command =>
-        Get_Interface_Type(obj, interface_type).Any(h => h.IsAssignableFrom(typeof(T)));
+        Get_Interface_Type(obj, interface_types).Any(h => h.IsAssignableFrom(typeof(T)));
 
     public static IEnumerable<Type> Get_Interface_Types(
         this Assembly assembly,
-        Type interface_type
-    ) => assembly.GetTypes().Where(t => Has_Interface(t, interface_type));
+        params Type[] interface_types
+    ) => assembly.GetTypes().Where(t => Has_Interface(t, interface_types));
 
-    private static bool Has_Interface(Type type, Type interface_type) =>
+    private static bool Has_Interface(Type type, Type[] interface_types) =>
         !type.IsAbstract
         && !type.IsInterface
-        && type.GetInterfaces().Any(t => Is_Interface(t, interface_type));
+        && type.GetInterfaces().Any(t => Is_Interface(t, interface_types));
 
-    private static IEnumerable<Type> Get_Interface_Type(object obj, Type interface_type) =>
+    private static IEnumerable<Type> Get_Interface_Type(object obj, Type[] interface_types) =>
         obj.GetType()
             .GetInterfaces()
-            .Where(i => Is_Interface(i, interface_type))
+            .Where(i => Is_Interface(i, interface_types))
             .Select(i => i.GetGenericArguments().First());
 
-    private static bool Is_Interface(Type type, Type interface_type) =>
-        type.IsGenericType && type.GetGenericTypeDefinition() == interface_type;
+    private static bool Is_Interface(Type type, Type[] interface_types) =>
+        type.IsGenericType && interface_types.Contains(type.GetGenericTypeDefinition());
 }
